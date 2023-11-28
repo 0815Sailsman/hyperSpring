08939b5d8f746e972e869062b99b3fd659a10922
0815Sailsman <sailsman@tutanota.com>
2023-11-29 00:00:58 +0100

    change entity constructor defaults

--- src/main/kotlin/com/sailsman/hyperspring/dsroute/Entities.kt
+++ src/main/kotlin/com/sailsman/hyperspring/dsroute/Entities.kt
@@ -5,37 +5,37 @@ import jakarta.persistence.*
 @Entity
 abstract class Unlocking (
     @Id @GeneratedValue var id: Long? = null,
-    @ManyToMany var unlocks: MutableList<Area> = mutableListOf()
+    @ManyToMany var unlocks: MutableList<Area>
 )
 
 @Entity
 class Enemy  (
     @Id @GeneratedValue override var id: Long? = null,
-    @ManyToMany override var unlocks: MutableList<Area> = mutableListOf(),
+    @ManyToMany override var unlocks: MutableList<Area>,
     var name: String,
     @ManyToOne var area: Area,
     var respawns: Boolean,
     var count: Int,
-    var amountKilled: Int,
+    var amountKilled: Int = 0,
 ) : Unlocking(id, unlocks)
 
 @Entity
 class Item  (
     @Id @GeneratedValue override var id: Long? = null,
-    @ManyToMany override var unlocks: MutableList<Area> = mutableListOf(),
+    @ManyToMany override var unlocks: MutableList<Area>,
     var name: String,
     @ManyToOne var area: Area,
     var count: Int,
-    var amountCollected: Int,
+    var amountCollected: Int = 0,
 ) : Unlocking(id, unlocks)
 
 @Entity
 class Area (
     @Id @GeneratedValue override var id: Long? = null,
-    @ManyToMany override var unlocks: MutableList<Area> = mutableListOf(),
+    @ManyToMany override var unlocks: MutableList<Area>,
     var name: String,
-    @ManyToMany var connections: MutableList<Area> = mutableListOf(),
-    @ManyToMany var dependencies: MutableList<Unlocking> = mutableListOf(),
-    @OneToMany var items: MutableList<Item> = mutableListOf(),
-    @OneToMany var enemies: MutableList<Enemy> = mutableListOf(),
+    @ManyToMany var connections: MutableList<Area>,
+    @ManyToMany var dependencies: MutableList<Unlocking>,
+    @OneToMany var items: MutableList<Item>,
+    @OneToMany var enemies: MutableList<Enemy>,
 ) : Unlocking(id, unlocks)
